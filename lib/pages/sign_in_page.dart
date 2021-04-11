aa2369380a7a7057558a6845591c10e2397031fc
offlineProgrammer <mmalaka@gmail.com>
2021-04-10 17:55:03 -0700

    Sign In button

--- lib/pages/sign_in_page.dart
+++ lib/pages/sign_in_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_signin_button/button_builder.dart';
+import 'package:flutter_signin_button/flutter_signin_button.dart';
 import 'package:provider/provider.dart';
 
 class SignInPage extends StatelessWidget {
@@ -78,86 +80,93 @@ class SignInPage extends StatelessWidget {
   }
 
   Widget _buildContent(BuildContext context) {
-    return Center(
-      child: Text('Let us do it'),
+    return Padding(
+      padding: EdgeInsets.all(16),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          SizedBox(
+            child: _buildHeader(),
+            height: 50.0,
+          ),
+          SizedBox(
+            height: 48.0,
+          ),
+          SignInButtonBuilder(
+            text: 'Sign in with email',
+            icon: Icons.email,
+            height: 60,
+            onPressed: () {
+              // _showButtonPressDialog(context, 'Email');
+            },
+            backgroundColor: Colors.blueGrey[700],
+            width: 220.0,
+          ),
+          SizedBox(
+            height: 8.0,
+          ),
+          SignInButton(
+            Buttons.Google,
+            padding: const EdgeInsets.symmetric(vertical: 8),
+            onPressed: () {
+              //_showButtonPressDialog(context, 'Google');
+            },
+          ),
+          SizedBox(
+            height: 8.0,
+          ),
+          SignInButton(
+            Buttons.FacebookNew,
+            padding: const EdgeInsets.symmetric(vertical: 15),
+            onPressed: () {
+              //_showButtonPressDialog(context, 'Google');
+            },
+          ),
+          SizedBox(
+            height: 8.0,
+          ),
+          SizedBox(
+            height: 8.0,
+          ),
+          Text(
+            'Or',
+            style: TextStyle(
+              fontSize: 14,
+              color: Colors.black87,
+            ),
+            textAlign: TextAlign.center,
+          ),
+          SizedBox(
+            height: 8.0,
+          ),
+          SignInButtonBuilder(
+            text: 'Go anonymous',
+            height: 60,
+            onPressed: () {
+              // _showButtonPressDialog(context, 'Email');
+            },
+            backgroundColor: Colors.lime[300],
+            textColor: Colors.black,
+          ),
+        ],
+      ),
     );
-    // return Padding(
-    //   padding: EdgeInsets.all(16),
-    //   child: Column(
-    //     mainAxisAlignment: MainAxisAlignment.center,
-    //     crossAxisAlignment: CrossAxisAlignment.stretch,
-    //     children: [
-    //       SizedBox(
-    //         child: _buildHeader(),
-    //         height: 50.0,
-    //       ),
-    //       SizedBox(
-    //         height: 48.0,
-    //       ),
-    //       SocialSignInButton(
-    //         assetName: 'images/google-logo.png',
-    //         text: 'Sign in with Google',
-    //         textColor: Colors.black87,
-    //         color: Colors.white,
-    //         onPressed: isLoading ? null : () => _signInWithGoogle(context),
-    //       ),
-    //       SizedBox(
-    //         height: 8.0,
-    //       ),
-    //       SocialSignInButton(
-    //         assetName: 'images/facebook-logo.png',
-    //         text: 'Sign in with Facebook',
-    //         textColor: Colors.white,
-    //         color: Color(0xFF334D92),
-    //         onPressed: isLoading ? null : () => _signInWithFacebook(context),
-    //       ),
-    //       SizedBox(
-    //         height: 8.0,
-    //       ),
-    //       SignInButton(
-    //         text: 'Sign in with email',
-    //         textColor: Colors.white,
-    //         color: Colors.teal.shade700,
-    //         onPressed: isLoading ? null : () => _signInWithEmail(context),
-    //       ),
-    //       SizedBox(
-    //         height: 8.0,
-    //       ),
-    //       Text(
-    //         'Or',
-    //         style: TextStyle(
-    //           fontSize: 14,
-    //           color: Colors.black87,
-    //         ),
-    //         textAlign: TextAlign.center,
-    //       ),
-    //       SizedBox(
-    //         height: 8.0,
-    //       ),
-    //       SignInButton(
-    //         text: 'Go anonymous',
-    //         textColor: Colors.black,
-    //         color: Colors.lime.shade300,
-    //         onPressed: isLoading ? null : () => _signInAnonymously(context),
-    //       ),
-    //     ],
-    //   ),
-    // );
   }
 
-  // Widget _buildHeader() {
-  //   if (isLoading) {
-  //     return Center(
-  //       child: CircularProgressIndicator(),
-  //     );
-  //   }
-  //   return Text(
-  //     'Sign In',
-  //     textAlign: TextAlign.center,
-  //     style: TextStyle(
-  //       fontSize: 32,
-  //       fontWeight: FontWeight.w600,
-  //     ),
-  //   );
-  // }
+  Widget _buildHeader() {
+    // if (isLoading) {
+    //   return Center(
+    //     child: CircularProgressIndicator(),
+    //   );
+    // }
+    return Text(
+      'Sign In',
+      textAlign: TextAlign.center,
+      style: TextStyle(
+        fontSize: 32,
+        fontWeight: FontWeight.w600,
+      ),
+    );
+  }
 }
